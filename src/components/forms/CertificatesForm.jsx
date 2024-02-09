c7c333c3630208af67a7351be5304e0c898a6c56
John Hansen <jhb.rgn@gmail.com>
2024-02-09 14:18:32 -0500

    CertificationsForm function ality complete

--- src/components/forms/CertificatesForm.jsx
+++ src/components/forms/CertificatesForm.jsx
@@ -8,31 +8,37 @@ const CertificatesForm = () => {
   const { isDirty, setIsDirty } = useStore();
   const { register, handleSubmit } = useForm();
   const [certifications, setCertifications] = useState([]);
-  const [certificationOptions, setCertificationOptions] = useState([]); 
+  const [certificationOptions, setCertificationOptions] = useState([]);
+
+  const fetchCertifications = async () => {
+    try {
+      const response = await Client.get('/api/certifications');
+      setCertifications(response.data || [])
+    } catch (error) {
+      console.error('Error fetching certification data:', error);
+    }
+  };
+
+  const fetchCertificationOptions = async () => {
+    try {
+      const response = await Client.get('/api/certifications/options');
+      setCertificationOptions(response.data)
+    } catch (error) {
+      console.error('Error fetching certifications options data:', error);
+    }
+  };
+
+  const deleteCertification = async (id) => {
+    try {
+      await Client.delete(`/api/certifications/${id}`);
+      fetchCertifications();
+    } catch (error) {
+      console.error('Error deleting certification:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchCertifications = async () => {
-      try {
-        const response = await Client.get('/api/certifications');
-        setCertifications(response.data || [])
-      } catch (error) {
-        console.error('Error fetching certification data:', error);
-      }
-    };
-
     fetchCertifications();
-  }, []);
-
-  useEffect(() => {
-    const fetchCertificationOptions = async () => {
-      try {
-        const response = await Client.get('/api/certifications/options');
-        setCertificationOptions(response.data)
-      } catch (error) {
-        console.error('Error fetching certifications options data:', error);
-      }
-    };
-
     fetchCertificationOptions();
   }, []);
 
@@ -54,6 +60,7 @@ const CertificatesForm = () => {
     try {
       await Client.post('/api/certifications', certificationData);
       setIsDirty(false);
+      fetchCertifications();
     } catch (error) {
       console.error('Error submitting form:', error);
     }
@@ -64,25 +71,32 @@ const CertificatesForm = () => {
     <div>
       <h2>Certifications</h2>
       <div>
-        {certifications.map(certification => {
-          return (
-            <div key={certification.id}>
-              <p>{certification.certificationRequirementId}</p>
-              <p>{certification.issueDate}</p>
-              <p>{certification.diveShop}</p>
-              <p>{certification.instructor}</p>
-              <p>{certification.instructorNo}</p>
-              <button>Delete</button>
-            </div>
-          );
-        })}
+        {certifications
+          .sort((a, b) => {
+            if (a.issueDate === b.issueDate) {
+              return b.id - a.id;
+            }
+            return new Date(b.issueDate) - new Date(a.issueDate);
+          })
+          .map(certification => {
+            return (
+              <div key={certification.id}>
+                <p>{certification.CertificationRequirement.name}</p>
+                <p>{certification.issueDate}</p>
+                <p>{certification.diveShop}</p>
+                <p>{certification.instructor}</p>
+                <p>{certification.instructorNo}</p>
+                <button onClick={() => deleteCertification(certification.id)}>Delete</button>
+              </div>
+            );
+          })}
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="certificationOptions">Certification:</label>  
-          <select id="certificationOptions" {...register('certificationRequirementId', { required: 'Certification is required' })} onChange={handleInputChange}> 
+          <label htmlFor="certificationOptions">Certification:</label>
+          <select id="certificationOptions" {...register('certificationRequirementId', { required: 'Certification is required' })} onChange={handleInputChange}>
             <option value="">Select Certification</option>
-            {certificationOptions.map(certification => {
+            {certificationOptions.sort((a, b) => a.name.localeCompare(b.name)).map(certification => {
               return (
                 <option key={certification.id} value={certification.id}>{certification.name}</option>
               );
@@ -96,6 +110,10 @@ const CertificatesForm = () => {
         <div>
           <label htmlFor="certificationDiveShop">Issuing Dive Shop:</label>
           <input id="certificationDiveShop" {...register('diveShop')} onChange={handleInputChange} />
+        </div>
+        <div>
+          <label htmlFor="certificationLocation">Location:</label>
+          <input id="certificationLocation" {...register('location')} onChange={handleInputChange} />
         </div>
         <div>
           <label htmlFor="certificationInstructor">Instructor:</label>
