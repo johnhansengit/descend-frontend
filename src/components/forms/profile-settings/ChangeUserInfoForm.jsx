b41142402831b2c2980e66479e4355be12667942
John Hansen <jhb.rgn@gmail.com>
2024-02-12 18:01:14 -0500

    Complete styling of change user credentials

--- src/components/forms/profile-settings/ChangeUserInfoForm.jsx
+++ src/components/forms/profile-settings/ChangeUserInfoForm.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { useStore } from '../../../services/store';
 import { ChangeUserName, ChangeEmail, ChangePassword, CheckUserName, CheckEmail } from '../../../services/Auth';
+import { TextField, Button, Box, Typography, Alert } from '@mui/material';
 
 const ChangeUserInfoForm = () => {
 
   const { user } = useStore();
 
-  const { handleSubmit, register, control, watch, setValue } = useForm();
+  const { handleSubmit, watch, setValue } = useForm();
   const newUserName = watch('newUserName');
   const [formValues, setFormValues] = useState({
     newUserName: '',
@@ -152,82 +153,108 @@ const ChangeUserInfoForm = () => {
 
 
   return (
-    <div>
-      <div>
-        <h2>Update Username, Email, or Password</h2>
-      </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="changeNewUserName">New Username</label>
-          <Controller
-            name="newUserName"
-            control={control}
-            defaultValue=""
-            render={({ field }) => (
-              <input
-                id="changeNewUserName"
-                type="text"
-                {...field}
-                onChange={(e) => {
-                  if (!/\s/.test(e.target.value)) {
-                    field.onChange(e);
-                  }
-                }}
-              />
-            )}
-          />
-          {userNameError && <p>{userNameError}</p>}
-        </div>
-        <div>
-          <label htmlFor="changeEmail">New Email</label>
-          <input
-            id="changeEmail"
-            type="email"
-            {...register('email')}
-            onChange={handleChange}
-          />
-          {emailError && <p>{emailError}</p>}
-        </div>
-        <div>
-          <label htmlFor="changeNewPassword">New Password</label>
-          <input
-            id="changeNewPassword"
-            type="password"
-            {...register('newPassword')}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="changeConfirmNewPassword">Confirm New Password</label>
-          <input
-            id="changeConfirmNewPassword"
-            type="password"
-            name="confirmNewPassword"
-            value={formValues.confirmNewPassword}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          {passwordErrors.length > 0 && (
-            <ul>
-              {passwordErrors.map((error, index) => (
-                <li key={index}>{error}</li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div>
-          <label htmlFor="changeCurrentPassword">Current Password (required to make changes)</label>
-          <input
-            id="changeCurrentPassword"
-            type="password"
-            {...register('password', { required: true })}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Update</button>
-      </form>
-    </div>
+    <Box
+      sx={{
+        backgroundColor: (theme) => theme.palette.foreground,
+        color: (theme) => theme.palette.text.primary,
+        fontFamily: (theme) => theme.typography.fontFamily,
+        p: 3,
+        borderRadius: 2,
+      }}
+    >
+      <Typography
+        variant="h4"
+        sx={{ mb: 2 }}
+      >
+        Update Username or Login
+      </Typography>
+      <TextField
+        margin="normal"
+        fullWidth
+        id="changeNewUserName"
+        label="New Username"
+        name="newUserName"
+        autoComplete="username"
+        autoFocus
+        value={formValues.newUserName}
+        onChange={handleChange}
+      />
+      {userNameError && <Alert severity="error">{userNameError}</Alert>}
+      <TextField
+        margin="normal"
+        fullWidth
+        id="changeEmail"
+        label="New Email"
+        name="email"
+        autoComplete="email"
+        value={formValues.email}
+        onChange={handleChange}
+      />
+      {emailError && <Alert severity="error">{emailError}</Alert>}
+      <TextField
+        margin="normal"
+        fullWidth
+        name="newPassword"
+        label="New Password"
+        type="password"
+        id="changeNewPassword"
+        value={formValues.newPassword}
+        onChange={handleChange}
+      />
+      <TextField
+        margin="normal"
+        fullWidth
+        name="confirmNewPassword"
+        label="Confirm New Password"
+        type="password"
+        id="changeConfirmNewPassword"
+        value={formValues.confirmNewPassword}
+        onChange={handleChange}
+      />
+      {passwordErrors.length > 0 && passwordErrors.map((error, index) => (
+        <Box width="100%" key={index}>
+          <Alert severity="error" >{error}</Alert>
+        </Box>
+      ))}
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Current Password (required to make changes)"
+        type="password"
+        id="changeCurrentPassword"
+        value={formValues.password}
+        onChange={handleChange}
+        sx={{
+          '& .MuiOutlinedInput-root': {
+            '& fieldset': { // Normal state
+              borderColor: (theme) => theme.palette.accent.main,
+              borderWidth: 3,
+            },
+            '&:hover fieldset': { // Hover state
+              borderColor: (theme) => theme.palette.accent.main,
+            },
+            '&.Mui-focused fieldset': { // Focused state
+              borderColor: (theme) => theme.palette.accent.main,
+            },
+          },
+        }}      
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{
+          mt: 3,
+          mb: 2,
+          backgroundColor: (theme) => theme.palette.accent.main,
+        }}
+        disabled={!formValues.password}
+      >
+        Update
+      </Button>
+    </Box>
   );
 };
 
